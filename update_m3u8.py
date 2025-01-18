1a4490a960912707a9618960dd345ce6a542176e
dtvabrand <marcobertolone@outlook.com>
2025-01-18 12:06:14 +0100

    Update update_m3u8.py

--- update_m3u8.py
+++ update_m3u8.py
@@ -2,5 +2,6 @@ import requests, re
 html = requests.get("https://www.youtube.com/watch?v=2Xn1Bb697A0", headers={'User-Agent': 'Mozilla/5.0'}).text
 new_m3u8 = re.search(r'"hlsManifestUrl":"(https://manifest\.googlevideo\.com.*?\.m3u8)"', html).group(1)
 file_url = "https://raw.githubusercontent.com/dtvabrand/Universal_Entertainment/refs/heads/main/playlist.m3u8"
-updated_content = re.sub(r'https://manifest\.googlevideo\.com.*?\.m3u8', new_m3u8, requests.get(file_url).text)
+file_content = requests.get(file_url).text
+updated_content = re.sub(r'https://manifest\.googlevideo\.com.*?\.m3u8', new_m3u8, file_content)
 requests.put(file_url, data=updated_content)
