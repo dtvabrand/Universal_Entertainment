8be519498bbc73668314c7c723e9f099b15c29a7
dtvabrand <marcobertolone@outlook.com>
2025-01-18 12:09:38 +0100

    Update update_m3u8.py

--- update_m3u8.py
+++ update_m3u8.py
@@ -1,7 +1,11 @@
 import requests, re
+
 html = requests.get("https://www.youtube.com/watch?v=2Xn1Bb697A0", headers={'User-Agent': 'Mozilla/5.0'}).text
-new_m3u8 = re.search(r'"hlsManifestUrl":"(https://manifest\.googlevideo\.com.*?\.m3u8)"', html).group(1)
-file_url = "https://raw.githubusercontent.com/dtvabrand/Universal_Entertainment/refs/heads/main/playlist.m3u8"
-file_content = requests.get(file_url).text
-updated_content = re.sub(r'https://manifest\.googlevideo\.com.*?\.m3u8', new_m3u8, file_content)
-requests.put(file_url, data=updated_content)
+print(html)  # Aggiungi per vedere il contenuto della pagina
+new_m3u8 = re.search(r'"hlsManifestUrl":"(https://manifest\.googlevideo\.com.*?\.m3u8)"', html)
+
+if new_m3u8:
+    new_m3u8 = new_m3u8.group(1)
+    print(f"Nuovo link m3u8 trovato: {new_m3u8}")  # Debug
+else:
+    print("Link m3u8 non trovato nel contenuto HTML")
