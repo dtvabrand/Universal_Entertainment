38d3c948ff98b75cce31a357931dda022d9bb92a
dtvabrand <marcobertolone@outlook.com>
2025-01-18 22:33:24 +0100

    Update and rename extract_cookies.py to extract_cookies_playwright.py

--- scripts/extract_cookies.py
+++
@@ -1,7 +0,0 @@
-import os
-import browser_cookie3
-
-cookies = browser_cookie3.chrome()
-with open('scripts/cookies.txt', 'w') as file:
-    for cookie in cookies:
-        file.write(f"{cookie.name}={cookie.value}\n")
