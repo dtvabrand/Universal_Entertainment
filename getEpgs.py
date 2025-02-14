fb1d0f845f69170727f16bf845a97cafff0b13e9
dtvabrand <marcobertolone@outlook.com>
2025-02-14 22:29:44 +0100

    Update getEpgs.py

--- getEpgs.py
+++ getEpgs.py
@@ -63,7 +63,7 @@ def filter_and_build_epg(urls):
 #m3u4u_epg = os.getenv("M3U4U_EPG")
 
 urls = [
-	'https://xmltv.tvkaista.net/guides/superguidatv.it.xml',
+	'http://www.epgitalia.tv/gzip',
 	'https://epgshare01.online/epgshare01/epg_ripper_US1.xml.gz',
 	'https://epgshare01.online/epgshare01/epg_ripper_US_LOCALS2.xml.gz',
 	'https://epgshare01.online/epgshare01/epg_ripper_UK1.xml.gz',
