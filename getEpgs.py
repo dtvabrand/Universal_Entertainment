b541b28378d1135e512c66bc41363255f7ed79ea
dtvabrand <marcobertolone@outlook.com>
2025-08-05 19:03:06 +0200

    Update getEpgs.py

--- getEpgs.py
+++ getEpgs.py
@@ -72,6 +72,7 @@ def filter_and_build_epg(urls):
 m3u4u_epg = os.getenv("M3U4U_EPG")
 
 urls = [
+    'http://www.epgitalia.tv/gzip',
     'https://epgshare01.online/epgshare01/epg_ripper_IT1.xml.gz',
     'https://epgshare01.online/epgshare01/epg_ripper_US1.xml.gz',
     'https://epgshare01.online/epgshare01/epg_ripper_US_LOCALS2.xml.gz',
@@ -80,3 +81,4 @@ urls = [
 
 if __name__ == "__main__":
     filter_and_build_epg(urls)
+
