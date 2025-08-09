343c81702e699317521944e8440a175d478cc120
dtvabrand <marcobertolone@outlook.com>
2025-08-09 14:09:08 +0200

    Update getEpgs.py

--- getEpgs.py
+++ getEpgs.py
@@ -73,6 +73,7 @@ m3u4u_epg = os.getenv("M3U4U_EPG")
 
 urls = [
     'http://www.epgitalia.tv/gzip',
+    'https://www.open-epg.com/files/italy1.xml.gz',
     'https://epgshare01.online/epgshare01/epg_ripper_IT1.xml.gz',
     'https://epgshare01.online/epgshare01/epg_ripper_US1.xml.gz',
     'https://epgshare01.online/epgshare01/epg_ripper_US_LOCALS2.xml.gz',
@@ -81,3 +82,4 @@ urls = [
 
 if __name__ == "__main__":
     filter_and_build_epg(urls)
+
